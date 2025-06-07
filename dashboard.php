14cb999c5223ff4077e93d8092051c7a9b59439e
Raj1411 <56500451+Raj1411@users.noreply.github.com>
2025-06-07 21:44:55 +0530

    dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -16,11 +16,9 @@ if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true) {
     <h2>Secure Power BI Dashboard</h2>
     <a href="logout.php">Logout</a>
     <iframe title="Power BI Dashboard" 
-        width="100%" 
-        height="600"
+        width="100%" height="600"
         src="https://app.powerbi.com/view?r=eyJrIjoiMzdmZWUxM2QtZGY4My00YjlkLWFjYjEtMzI2NjY3YjdmNmFlIiwidCI6IjU2NjU2NjEzLTBkZWEtNGU3OC1iNWVlLWRhNjI3NGZmMjZkZSJ9" 
-        frameborder="0" 
-        allowFullScreen="true">
+        frameborder="0" allowFullScreen="true">
         </iframe>
 </div>
 </body>
